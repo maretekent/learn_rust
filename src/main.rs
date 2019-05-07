dfdf0acb63225a561059aaad9f6fc2ce80516195
kenneth.marete <maretekent@gmail.com>
2019-05-07 21:35:07 +0300

    Validating request and returning response

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,26 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    // let response = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n<html><body>Hello world</body></html>\r\n";
-    
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    if buffer.starts_with(get){
+        let contents = fs::read_to_string("hello.html").unwrap();
 
-    match stream.write(response.as_bytes()){
-        Ok(_) => println!("Response content sent"),
-        Err(e) => println!("Response failed, error : {}", e),
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        match stream.write(response.as_bytes()){
+            Ok(_) => println!("Response content sent"),
+            Err(e) => println!("Response failed, error : {}", e),
+        }
+        stream.flush().unwrap();
+    } else  {
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        match stream.write(response.as_bytes()){
+            Ok(_) => println!("Response sent"),
+            Err(e) => println!("Response failed, error : {}", e),
+        }
     }
-    stream.flush().unwrap();
 }
